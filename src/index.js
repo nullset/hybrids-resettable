ab040c9a23f38fd7232ea3f799c630c508ce2207
Nathan Wright <nathan@brandalism.com>
2020-05-08 02:28:13 -0500

    yarn add initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+export default function resettable({ ignore = [] }) {
+  let initialValues;
+  return {
+    connect: (host, key) => {
+      // Ignore all specified keys, this particular key (often "reset"), as well as the render key.
+      ignore.push(key);
+      ignore.push('render');
 
-import confetti from "canvas-confetti";
+      // Set a value to trigger the observer.
+      host[key] = true;
+    },
+    set: (host, value) => value,
+    get: (host) => () => {
+      // Return a function that, when called, resets all non-ignored keys.
+      initialValues.forEach((value, key) => {
+        host[key] = value;
+      });
+      return true;
+    },
+    observe: (host, value, oldValue) => {
+      // Only set the initialValues if they've never been set before.
+      if (!oldValue) {
+        initialValues = Object.keys(Object.getPrototypeOf(host)).reduce(
+          (acc, protoKey) => {
+            if (
+              !ignore.includes(protoKey) &&
+              !['undefined', 'function'].includes(typeof host[protoKey])
+            ) {
+              acc.set(protoKey, host[protoKey]);
+            }
 
-const canvas = document.createElement("canvas");
-canvas.width = "960";
-canvas.height = "960";
-document.body.appendChild(canvas);
-
-confetti.create(canvas, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+            return acc;
+          },
+          new Map()
+        );
+      }
+    },
+  };
+}
